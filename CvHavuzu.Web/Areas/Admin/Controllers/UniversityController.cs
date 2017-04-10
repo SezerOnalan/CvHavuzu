e09a809049e899e99bcf5a7023e82d5003da8db8
Murat Yanar <murat.yanar@WINDOWSLIVE.COM>
2017-04-10 13:01:19 +0300

    univertsity modeli ve türkçeleştirme tasarım iyileştirme

---
+++ CvHavuzu.Web/Areas/Admin/Controllers/UniversityController.cs
@@ -0,0 +1,154 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CvHavuzu.Web.Data;
+using CvHavuzu.Web.Models;
+
+namespace CvHavuzu.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UniversityController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UniversityController(ApplicationDbContext context)
+        {
+            _context = context;    
+        }
+
+        // GET: Admin/University
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Universities.ToListAsync());
+        }
+
+        // GET: Admin/University/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var university = await _context.Universities
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (university == null)
+            {
+                return NotFound();
+            }
+
+            return View(university);
+        }
+
+        // GET: Admin/University/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/University/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] University university)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(university);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(university);
+        }
+
+        // GET: Admin/University/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var university = await _context.Universities.SingleOrDefaultAsync(m => m.Id == id);
+            if (university == null)
+            {
+                return NotFound();
+            }
+            return View(university);
+        }
+
+        // POST: Admin/University/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] University university)
+        {
+            if (id != university.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(university);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UniversityExists(university.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(university);
+        }
+
+        // GET: Admin/University/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var university = await _context.Universities
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (university == null)
+            {
+                return NotFound();
+            }
+
+            return View(university);
+        }
+
+        // POST: Admin/University/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var university = await _context.Universities.SingleOrDefaultAsync(m => m.Id == id);
+            _context.Universities.Remove(university);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        private bool UniversityExists(int id)
+        {
+            return _context.Universities.Any(e => e.Id == id);
+        }
+    }
+}
