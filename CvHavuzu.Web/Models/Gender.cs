eace6244de643e29a9e8c8d10e95752fec6540ac
Mustafa Fındık <mustafa.findik3@gmail.com>
2017-04-18 10:27:00 +0300

    Gender And Age Fix On Homepage

--- CvHavuzu.Web/Models/Gender.cs
+++ CvHavuzu.Web/Models/Gender.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,10 @@ namespace CvHavuzu.Web.Models
 {
     public enum Gender
     {
+        [Display(Name = "Erkek")]
         Male = 1,
+
+        [Display(Name = "Kadın")]
         Female = 2
     }
 }
