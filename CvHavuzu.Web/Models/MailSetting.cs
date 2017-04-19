53859b54967cc69ec7893e970e2e1abd0d22af10
OnurKapici <onurkapici@gmail.com>
2017-04-19 11:25:44 +0300

    MailSettingAdded and ContacrPage Changed

---
+++ CvHavuzu.Web/Models/MailSetting.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CvHavuzu.Web.Models
+{
+    public class MailSetting
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(200)]
+        [Display(Name = "Gönderen Mail Adresi")]
+        public string FromAddress { get; set; }
+        [Required]
+        [StringLength(200)]
+        [Display(Name = "Gönderen Mail Şifresi")]
+        public string FromAddressPassword { get; set; }
+        [Required]
+        [StringLength(200)]
+        [Display(Name = "Gönderen Adı")]
+        public string FromAddressTitle { get; set; }
+        [Required]
+        [StringLength(200)]
+        [Display(Name = "Mailin Konusu")]
+        public string Subject { get; set; }
+        [Required]
+        [StringLength(200)]
+        [Display(Name = "Mailin İçeriği")]
+        public string BodyContent { get; set; }
+        [Required]
+        [StringLength(200)]
+        [Display(Name = "Mail Server Adresi")]
+        public string SmptServer { get; set; }
+        [Required]
+        [Display(Name = "Port Numarası")]
+        public int SmptPortNumber { get; set; }
+    }
+}
