a6bac4dfbbd9b23fc23335b50a66367d42b1810b
Yağmur Kılıç <yagmurkilic8@gmail.com>
2017-04-21 12:06:12 +0300

    .

--- CvHavuzu.Web/Areas/Admin/Controllers/SettingController.cs
+++ CvHavuzu.Web/Areas/Admin/Controllers/SettingController.cs
@@ -52,6 +52,10 @@ namespace CvHavuzu.Web.Areas.Admin.Controllers
                     s.SeoTitle = setting.SeoTitle;
                     s.SeoDescription = setting.SeoDescription;
                     s.SeoKeywords = setting.SeoKeywords;
+                    s.Phone = setting.Phone;
+                    s.Fax = setting.Fax;
+                    s.Address = setting.Address;
+                    s.Mail = setting.Mail;
                     s.Facebook = setting.Facebook;
                     s.Twitter = setting.Twitter;
                     s.LinkedIn = setting.LinkedIn;
