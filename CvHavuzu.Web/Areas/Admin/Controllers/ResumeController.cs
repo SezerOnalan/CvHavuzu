fc4d4c3aa3c91ff36daaeb1eab36b46022bd5ec5
ndrgcgl <gecgelnadir@gmail.com>
2017-04-25 13:17:24 +0300

    Bugs fixed

--- CvHavuzu.Web/Areas/Admin/Controllers/ResumeController.cs
+++ CvHavuzu.Web/Areas/Admin/Controllers/ResumeController.cs
@@ -20,8 +20,8 @@ namespace CvHavuzu.Web.Areas.Admin.Controllers
         private IHostingEnvironment env;
 
         private string FDir_AppData = "~/App_Data/";
-        
-       
+
+
         public ResumeController(IHostingEnvironment _env, ApplicationDbContext context, Data.ApplicationDbContext _contxt)
         {
             _context = context;
@@ -38,11 +38,11 @@ namespace CvHavuzu.Web.Areas.Admin.Controllers
             }
             else if (tab == 2)
             {
-                resumes = _context.Resumes.Include(r => r.Consultant).Include(r => r.Department).Include(r => r.EducationLevel).Include(r => r.Profession).Include(r => r.ResumeStatus).Include(r => r.City).Include(r => r.District).Include(r => r.Teacher).Include(r => r.University).Where(r=>r.ShowInList==true && r.Approved==true).ToList();
+                resumes = _context.Resumes.Include(r => r.Consultant).Include(r => r.Department).Include(r => r.EducationLevel).Include(r => r.Profession).Include(r => r.ResumeStatus).Include(r => r.City).Include(r => r.District).Include(r => r.Teacher).Include(r => r.University).Where(r => r.ShowInList == true && r.Approved == true).ToList();
             }
             else
             {
-                resumes = _context.Resumes.Include(r => r.Consultant).Include(r => r.Department).Include(r => r.EducationLevel).Include(r => r.Profession).Include(r => r.ResumeStatus).Include(r => r.City).Include(r=>r.District).Include(r => r.Teacher).Include(r => r.University).Where(r => r.Approved == false).ToList();
+                resumes = _context.Resumes.Include(r => r.Consultant).Include(r => r.Department).Include(r => r.EducationLevel).Include(r => r.Profession).Include(r => r.ResumeStatus).Include(r => r.City).Include(r => r.District).Include(r => r.Teacher).Include(r => r.University).Where(r => r.Approved == false).ToList();
             }
             ViewBag.tab = tab;
             return View(resumes);
@@ -65,7 +65,7 @@ namespace CvHavuzu.Web.Areas.Admin.Controllers
                 .Include(r => r.Teacher)
                 .Include(r => r.University)
                 .Include(r => r.City)
-                .Include(r=> r.District)
+                .Include(r => r.District)
                 .SingleOrDefaultAsync(m => m.Id == id);
             if (resume == null)
             {
@@ -78,6 +78,7 @@ namespace CvHavuzu.Web.Areas.Admin.Controllers
         // GET: Admin/Resume/Create
         public IActionResult Create()
         {
+           
             ViewData["ConsultantId"] = new SelectList(_context.Consultants, "Id", "Fullname");
             ViewData["DepartmentId"] = new SelectList(_context.Departments, "Id", "Name");
             ViewData["EducationLevelId"] = new SelectList(_context.EducationLevels, "Id", "Name");
@@ -97,13 +98,14 @@ namespace CvHavuzu.Web.Areas.Admin.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(Resume resume, IFormFile imageUpload, IFormFile resumeUpload)
         {
-            if (imageUpload != null && imageUpload.Length > 0 && ".gif,.jpg,.jpeg,.png".Contains(Path.GetExtension(imageUpload.FileName)) == false || imageUpload == null)
+            
+            if (imageUpload != null && imageUpload.Length > 0 && ".gif,.jpg,.jpeg,.png".Contains(Path.GetExtension(imageUpload.FileName)) == false)
             {
-                ModelState.AddModelError("ImageUpload", "Resim dosyasý uzantýsý .gif, .jpg, .jpeg ya da .png olmalýdýr ve alan boþ olamaz");
+                ModelState.AddModelError("ImageUpload", "Resim dosyasý uzantýsý .gif, .jpg, .jpeg ya da .png olmalýdýr.");
             }
             if (resumeUpload != null && resumeUpload.Length > 0 && ".pdf,.doc,.docx".Contains(Path.GetExtension(resumeUpload.FileName)) == false || resumeUpload == null)
             {
-                ModelState.AddModelError("ResumeUpload", "Cv dosyasý uzantýsý .doc, .docx ya da .pdf olmalýdýr ve alan boþ olamaz");
+                ModelState.AddModelError("ResumeUpload", "Cv dosyasý uzantýsý .doc, .docx ya da .pdf olmalýdýr ve alan boþ olamaz.");
             }
             else if (ModelState.IsValid)
             {
@@ -111,20 +113,21 @@ namespace CvHavuzu.Web.Areas.Admin.Controllers
 
                 if (imageUpload != null && imageUpload.Length > 0)
                 {
-                    
-                        var filePath = new Random().Next(9999).ToString() + imageUpload.FileName;
-                        using (var stream = new FileStream(env.WebRootPath + "\\uploads\\resumes\\images\\" + filePath, FileMode.Create))
-                        {
-                            imageUpload.CopyTo(stream);
-                        }
-                        resume.ImagePath = filePath;
-                   
-                }
 
+                    var filePath = new Random().Next(9999).ToString() + imageUpload.FileName;
+                   
+                    using (var stream = new FileStream(env.WebRootPath + "\\uploads\\resumes\\images\\" + filePath, FileMode.Create))
+                    {
+                        imageUpload.CopyTo(stream);
+                    }
+                    resume.ImagePath = filePath;
+
+                }
 
                 if (resumeUpload != null && resumeUpload.Length > 0)
                 {
                     var filePath = new Random().Next(9999).ToString() + resumeUpload.FileName;
+                    filePath = ReplaceTurksihEnglishCharacter(filePath);
                     using (var stream = new FileStream(env.WebRootPath + "\\uploads\\resumes\\" + filePath, FileMode.Create))
                     {
                         resumeUpload.CopyTo(stream);
@@ -140,7 +143,7 @@ namespace CvHavuzu.Web.Areas.Admin.Controllers
             ViewData["EducationLevelId"] = new SelectList(_context.EducationLevels, "Id", "Name", resume.EducationLevelId);
             ViewData["ProfessionId"] = new SelectList(_context.Professions, "Id", "Name", resume.ProfessionId);
             ViewData["ResumeStatusId"] = new SelectList(_context.ResumesStatuses, "Id", "Name", resume.ResumeStatusId);
-            ViewData["CityId"] = new SelectList(_context.Cities, "Id", "Name",resume.CityId);
+            ViewData["CityId"] = new SelectList(_context.Cities, "Id", "Name", resume.CityId);
             ViewData["DistrictId"] = new SelectList(_context.Districts, "Id", "Name", resume.DistrictId);
             ViewData["TeacherId"] = new SelectList(_context.Teachers, "Id", "Fullname", resume.TeacherId);
             ViewData["UniversityId"] = new SelectList(_context.Universities, "Id", "Name", resume.UniversityId);
@@ -179,6 +182,7 @@ namespace CvHavuzu.Web.Areas.Admin.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, Resume resume, IFormFile imageUpload, IFormFile resumeUpload)
         {
+            resume.UpdateDate = DateTime.Now;
             if (id != resume.Id)
             {
                 return NotFound();
@@ -194,25 +198,30 @@ namespace CvHavuzu.Web.Areas.Admin.Controllers
             else if (ModelState.IsValid)
             {
                 // file edit iþlemi yapýlýr
-              
+
                 try
                 {
                     // file upload iþlemi yapýlýr
 
-                        if (imageUpload != null && imageUpload.Length > 0)
+                    if (imageUpload != null && imageUpload.Length > 0)
+                    {
+                        var filePath = new Random().Next(9999).ToString() + imageUpload.FileName;
+                        using (var stream = new FileStream(env.WebRootPath + "\\uploads\\resumes\\images\\" + filePath, FileMode.Create))
                         {
-                            var filePath = new Random().Next(9999).ToString() + imageUpload.FileName;
-                            using (var stream = new FileStream(env.WebRootPath + "\\uploads\\resumes\\images\\" + filePath, FileMode.Create))
-                            {
-                                imageUpload.CopyTo(stream);
-                            }
-                            resume.ImagePath = filePath;
+                            imageUpload.CopyTo(stream);
                         }
+                        resume.ImagePath = filePath;
+                    }
+
+
 
 
                     if (resumeUpload != null && resumeUpload.Length > 0)
                     {
                         var filePath = new Random().Next(9999).ToString() + resumeUpload.FileName;
+
+                        filePath= ReplaceTurksihEnglishCharacter(filePath);
+
                         using (var stream = new FileStream(env.WebRootPath + "\\uploads\\resumes\\" + filePath, FileMode.Create))
                         {
                             resumeUpload.CopyTo(stream);
@@ -264,8 +273,8 @@ namespace CvHavuzu.Web.Areas.Admin.Controllers
                 .Include(r => r.EducationLevel)
                 .Include(r => r.Profession)
                 .Include(r => r.ResumeStatus)
-                .Include(r=>r.City)
-                .Include(r=> r.District)
+                .Include(r => r.City)
+                .Include(r => r.District)
                 .Include(r => r.Teacher)
                 .Include(r => r.University)
                 .SingleOrDefaultAsync(m => m.Id == id);
@@ -284,10 +293,11 @@ namespace CvHavuzu.Web.Areas.Admin.Controllers
         {
             var resume = await _context.Resumes.SingleOrDefaultAsync(m => m.Id == id);
 
-            try { 
-            _context.Resumes.Remove(resume);
-            await _context.SaveChangesAsync();
-            return RedirectToAction("Index");
+            try
+            {
+                _context.Resumes.Remove(resume);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
             }
             catch (Exception)
             {
@@ -301,6 +311,21 @@ namespace CvHavuzu.Web.Areas.Admin.Controllers
             return _context.Resumes.Any(e => e.Id == id);
         }
 
-
+        public static string  ReplaceTurksihEnglishCharacter(string FileName)
+        {
+            FileName = FileName.Replace('ö', 'o');
+            FileName = FileName.Replace('ü', 'u');
+            FileName = FileName.Replace('ð', 'g');
+            FileName = FileName.Replace('þ', 's');
+            FileName = FileName.Replace('ý', 'i');
+            FileName = FileName.Replace('ç', 'c');
+            FileName = FileName.Replace('Ö', 'O');
+            FileName = FileName.Replace('Ü', 'U');
+            FileName = FileName.Replace('Ð', 'G');
+            FileName = FileName.Replace('Þ', 'S');
+            FileName = FileName.Replace('Ý', 'I');
+            FileName = FileName.Replace('Ç', 'C');
+            return FileName;
+        }
     }
 }
--- CvHavuzu.Web/Data/Migrations/20170424093812_CvcV.Designer.cs
+++ CvHavuzu.Web/Data/Migrations/20170424093812_CvcV.Designer.cs
@@ -9,9 +9,10 @@ using CvHavuzu.Web.Models;
 namespace CvHavuzu.Web.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170424093812_CvcV")]
+    partial class CvcV
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.1")
